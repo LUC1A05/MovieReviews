ba7c0b3ee75fb4202d61e9995db80ba691c0653e
Asier Barrio <164157230+AsierBarrio@users.noreply.github.com>
2026-03-28 19:23:28 +0100

    Script con fors

--- script.sh
+++ script.sh
@@ -7,12 +7,33 @@ CP=".;$EXEKUTAGARRIA"
 
 # Ajustarlo al PATH de cada uno
 MOVIES_PATH="C:\Users\asier\OneDrive\Escritorio\movies_reviews"
+MAIN="MainDatuZientzia"
+
+PARAM1=(500, 1000, 1500)
+PARAM2=(0, 1)
+PARAM3=(0, 1)
+PARAM4=(0, 1)
+PARAM5=(0, 1)
+PARAM6=(1000, 1500, 2000)
 
 echo "--- MainDatuZientzia exekutatzen ---"
-echo "Usando Classpath: $CP"
 
-"$JAVA_EXEC" --add-opens java.base/java.lang=ALL-UNNAMED \
-            -cp "$CP" \
-            MainDatuZientzia $MOVIES_PATH
+for p1 in "${PARAM1[@]}"; do
+    for p2 in "${PARAM2[@]}"; do
+        for p3 in "${PARAM3[@]}"; do
+            for p4 in "${PARAM4[@]}"; do
+                for p5 in "${PARAM5[@]}"; do
+                    for p6 in "${PARAM6[@]}"; do
+
+				"$JAVA_EXEC" --add-opens java.base/java.lang=ALL-UNNAMED \
+            					-cp "$CP" \
+            					"$MAIN" $MOVIES_PATH "$p1" "$p2" "$p3" "$p4" "$p5" "$p6"
+
+			done
+		done
+	done
+	done
+done
+done
 
 echo "--- Ejecución finalizada ---"
