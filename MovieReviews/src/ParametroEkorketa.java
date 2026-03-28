bb1b2f37456b465801a816210788588c02f7c44e
AsierBarrio <asierbarrioborge10@gmail.com>
2026-03-28 15:06:19 +0100

    Parametro ekortzeko datu gehigarriak gehituta

--- MovieReviews/src/ParametroEkorketa.java
+++ MovieReviews/src/ParametroEkorketa.java
@@ -13,19 +13,19 @@ public class ParametroEkorketa {
 	 * @param dev
 	 * @throws Exception
 	 */
-	public void parametroEkorketa(Instances train, Instances dev) throws Exception {
-//	public static void main(String args[]) throws Exception{
+//	public void parametroEkorketa(Instances train, Instances dev) throws Exception {
+	public static void main(String args[]) throws Exception{
 
-//		DataSource tSource = new DataSource("train_aurreprozesatuta.arff");
-//		DataSource tDev = new DataSource("dev_aurreprozesatuta.arff");
-//		
-//		Instances train = tSource.getDataSet();
-//		Instances dev = tDev.getDataSet();
-//		
-//		train.setClassIndex(train.numAttributes() - 1);
-//		dev.setClassIndex(dev.numAttributes() - 1);
-//		train.classAttribute().setStringValue("Klasea");
-//		
+		DataSource tSource = new DataSource("train_aurreprozesatuta.arff");
+		DataSource tDev = new DataSource("dev_aurreprozesatuta.arff");
+		
+		Instances train = tSource.getDataSet();
+		Instances dev = tDev.getDataSet();
+		
+		train.setClassIndex(train.numAttributes() - 1);
+		dev.setClassIndex(dev.numAttributes() - 1);
+		train.classAttribute().setStringValue("Klasea");
+		
 //		//Lo tengo puesto para hacer pruebas
 //		StringToWordVector stwv = new StringToWordVector();
 //		//stwv.setAttributeNamePrefix("W_");
@@ -33,20 +33,21 @@ public class ParametroEkorketa {
 //		Instances tVector = Filter.useFilter(train, stwv);
 //		Instances dVector = Filter.useFilter(dev, stwv);
 //		/////////////////////////////////////////////////
-//		
-//		System.out.println(tVector.classAttribute().value(0));
-//		System.out.println(dVector.classAttribute().value(1));
-//		
-//		System.out.println(train.numAttributes());
-//		System.out.println(dev.numAttributes());
+		
+		System.out.println(train.numInstances());
+		System.out.println(dev.numAttributes());
 		
 		//Valores de prueba, luego pondré los "reales"
 		//AdaBoostM1
-		int[] iterazioak = {10, 50, 100}; 
+		int[] iterazioak = {10, 50, 100, 250, 500, 1000}; 
 		int[] threshold = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 		
 		//J48
 		float[] confidence = {(float) 0.1, (float) 0.25, (float) 0.5}; 
+		int n = train.numInstances();
+		double maxHosto = n * 0.05;
+		double minHosto = n * 0.01;
+		
 		
 		double maxFMeasure = 0;
 		int itOnena = 0;
@@ -60,8 +61,8 @@ public class ParametroEkorketa {
 				System.out.println("Threshold: " + t);
 				for(float c : confidence) {
 					System.out.println("Confidence: " + c);
-					for(int h=1; h<=train.numInstances(); h++) {
-						
+					for(int h=(int) minHosto; h<=maxHosto; h++) {
+						System.out.println("Hostoa: " + h);
 						OptimalModelCreator.getOpc().setParametroOptimoak(i, t, c, h);
 						AdaBoostM1 adaboost = (AdaBoostM1) OptimalModelCreator.getOpc().entrenatuEreduOptimoa(train);
 						
